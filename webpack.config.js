99aba0bff43b705005048bdea3a3275d60974e40
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-06-12 12:08:09 +0100

    webpack-config.js refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+import { resolve } from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
-module.exports = {
-  mode: "development",
-  entry: "./app/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js",
-  },
-  module: {
-    rules: [
-      { test: /\.(js)$/, use: "babel-loader" },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ] 
+export const mode = "development"
+export const entry = "./app/index.js"
+export const output = {
+  path: resolve(__dirname, "dist"),
+  filename: "index_bundle.js",
 }
+export const module = {
+  rules: [
+    { test: /\.(js)$/, use: "babel-loader" },
+    { test: /\.css$/, use: ["style-loader", "css-loader"] },
+  ],
+}
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'app/index.html'
+  })
+]
 
